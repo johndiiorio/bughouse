4808f8227c5de3d7c5665279dab6067002d85447
John DiIorio <johnzdiiorio@gmail.com>
2016-02-21 01:33:50 -0600

    Added basic test MySQL connections and a db model.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'jzdMySQL88',
+    database: 'bughouse_db'
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    var conn = connection.connect();
+    connection.query('SELECT * from users', function (err, rows, fields) {
+        if (!err) {
+            res.send(rows);
+        }
+        else {
+            console.log('Error while performing Query.');
+        }
+    });
+    connection.end();
 });
 
 module.exports = router;
