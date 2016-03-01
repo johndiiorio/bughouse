b8b379c52174b2d415628cd3cc462568cbac28d7
John DiIorio <johnzdiiorio@gmail.com>
2016-03-01 16:56:23 -0600

    Began adding UI elements to home page (sliders, switches, buttons). Used two libraries (switches and sliders).

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -4,4 +4,15 @@ app.controller('homeController', function ($scope, $http) {
         $("#notificationSuccess").hide();
         $("#notificationError").hide();
     });
+    $('#minutesSlider').slider().on('slide', function (ev) {
+        $('#minutesDisplay').text("Minutes: " + ev.value);
+    });
+    $('#incrementSlider').slider().on('slide', function (ev) {
+        $('#incrementDisplay').text("Increment (seconds): " + ev.value);
+    });
+    //$('.btn').click(function() {
+    //    $('.btn').removeClass('btn-primary').addClass('btn-secondary');
+    //    $(this).addClass('btn-primary').removeClass('btn-secondary');
+    //});
+    $('#randomSlider').bootstrapSwitch();
 });
