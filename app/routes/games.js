8787badbf3618b923fb9579b8d16d9bccdcac760
John DiIorio <johnzdiiorio@gmail.com>
2016-07-24 14:34:21 -0700

    Added columns in db for start_time_left, start_time_right, and clocks. Refactored update game code in socket.io to new file. Set start_time_left/right on first move of board. Changed UI fonts.

--- app/routes/games.js
+++ app/routes/games.js
@@ -124,13 +124,13 @@ router.post('/', function (req, res) {
             res.json({"code": 100, "status": "Error in connection database"});
             return;
         }
-        function twoDigits(d) {
-            if (0 <= d && d < 10) return "0" + d.toString();
-            if (-10 < d && d < 0) return "-0" + (-1 * d).toString();
-            return d.toString();
-        }
 
         Date.prototype.toMysqlFormat = function () {
+            function twoDigits(d) {
+                if (0 <= d && d < 10) return "0" + d.toString();
+                if (-10 < d && d < 0) return "-0" + (-1 * d).toString();
+                return d.toString();
+            }
             return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
         };
         var currentTime = new Date().toMysqlFormat();
