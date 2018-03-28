013f600a26b4e8d61875e970330914977f1ab7c2
John DiIorio <johnzdiiorio@gmail.com>
2018-03-27 21:29:17 -0500

    Add a registration timestamp column to the users table

--- src/server/sql/user/create_new_user.sql
+++ src/server/sql/user/create_new_user.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, email, password_hash)
-VALUES (${username}, ${email}, ${passwordHash})
+INSERT INTO users (username, email, password_hash, registration_timestamp)
+VALUES (${username}, ${email}, ${passwordHash}, now())
 ON CONFLICT(username) DO NOTHING
 RETURNING id;
